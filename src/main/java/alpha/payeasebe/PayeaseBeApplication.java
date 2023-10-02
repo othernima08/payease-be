9f637e2678a3ff18b716221eda4673dcd21af8b8
Desi Christine Othernima <desichristine2@gmail.com>
2023-10-02 11:22:37 +0700

    feat: init repo - payease backend

---
+++ src/main/java/alpha/payeasebe/PayeaseBeApplication.java
@@ -0,0 +1,13 @@
+package alpha.payeasebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayeaseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayeaseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alpha/payeasebe/PayeaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package alpha.payeasebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayeaseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
