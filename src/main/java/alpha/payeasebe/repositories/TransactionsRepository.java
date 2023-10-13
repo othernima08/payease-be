5d5c58920e275fec6606a34ebcc025d392ec85b4
Trisna Bayu <btrisna90@gmail.com>
2023-10-13 10:54:29 +0700

    history transaction

--- src/main/java/alpha/payeasebe/repositories/TransactionsRepository.java
+++ src/main/java/alpha/payeasebe/repositories/TransactionsRepository.java
@@ -14,6 +14,6 @@ public interface TransactionsRepository extends JpaRepository<Transactions, Stri
     @Query(value = "SELECT t.id, t.amount, t.transaction_time, CASE WHEN t.already_done = true THEN 'Success' ELSE 'Pending' END AS status FROM transactions t JOIN transaction_categories tc ON t.transaction_categories_id = tc.id JOIN users u ON t.user_id = u.id WHERE tc.type = 'Top Up' AND t.is_deleted = false AND t.user_id = ? GROUP BY t.id", nativeQuery = true)
     List<ResponseShowTopUpHistory> getTopUpHistoryByUserId(String userId);
 
-    @Query(value = "SELECT u.first_name, u.last_name, t.amount, tc.type FROM Users u, Transactions t, Transaction_categories tc WHERE t.user_id = u.id AND t.transaction_categories_id = tc.id AND t.already_done = TRUE AND t.id = (SELECT id FROM Transactions WHERE user_id = t.user_id ORDER BY transaction_time DESC LIMIT 1)", nativeQuery = true)
+    @Query(value = "SELECT u.first_name, u.last_name,t.user_id,  t.amount, tc.type FROM Users u, Transactions t, Transaction_categories tc WHERE t.user_id = u.id AND t.transaction_categories_id = tc.id AND t.already_done = TRUE AND t.id = (SELECT id FROM Transactions WHERE user_id = t.user_id ORDER BY transaction_time DESC LIMIT 1)", nativeQuery = true)
     List<ResponseHistoryTransaction> getHistory(String UserId);
 }
