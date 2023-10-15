c0672de265b2cc3ebcda987a0f59117ff3104a81
Desi Christine Othernima <desichristine2@gmail.com>
2023-10-15 15:44:51 +0700

    feat: refactor

--- src/main/java/alpha/payeasebe/services/transactions/TransactionsService.java
+++ src/main/java/alpha/payeasebe/services/transactions/TransactionsService.java
@@ -9,18 +9,17 @@ import alpha.payeasebe.payloads.req.Transactions.TransferRequest;
 public interface TransactionsService {
     Transactions createTransactionService(String userId, String transactionCategoryName, Double amount);
 
-    ResponseEntity<?> getTransactionByUserId(String userId);
-    ResponseEntity<?> topUpService(TopUpRequest request);
+    ResponseEntity<?> getTopFiveTransactionByUserId(String userId);
+    
     ResponseEntity<?> transferDetail(String id);
     ResponseEntity<?> topUpGenerateCodeService(TopUpRequest request);
     ResponseEntity<?> topUpPaymentCodeService(String paymentCode);
 
-
     ResponseEntity<?> transferService(TransferRequest request);
+
     ResponseEntity<?> getTopUpHistoryByUserIdService(String userId);
     ResponseEntity<?> getTopUpHistoryByUserIdAndStatusService(String userId, Boolean isDeleted);
+
     ResponseEntity<?> getTransactionHistoryByUserIdService(String userId);
-    // ResponseEntity<?> getIncomeTransactionHistoryByUserIdService(String userId);
-    // ResponseEntity<?> getExpenseTransactionHistoryByUserIdService(String userId);
     ResponseEntity<?> getTransactionHistoryByUserIdAndTypeService(String userId, Boolean isIncome);
 }
