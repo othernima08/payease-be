b3d1cefe9ef2836cf2fb5158cd249f2990e83645
Angga Kahaerul <anggakahaerul@upi.edu>
2023-10-15 11:39:58 +0700

    Otp backend fixed add isVerified to user model

--- src/main/java/alpha/payeasebe/services/otp/OTPServicesImpl.java
+++ src/main/java/alpha/payeasebe/services/otp/OTPServicesImpl.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Service;
 
 import alpha.payeasebe.models.OTP;
 import alpha.payeasebe.models.User;
+import alpha.payeasebe.payloads.req.MailRequest;
 import alpha.payeasebe.payloads.req.OTP.CreateOTPRequest;
 import alpha.payeasebe.payloads.req.OTP.VerifyOTPRequest;
 import alpha.payeasebe.payloads.res.ResponseHandler;
 import alpha.payeasebe.repositories.OTPRepository;
 import alpha.payeasebe.repositories.UserRepository;
+import alpha.payeasebe.services.mail.MailService;
 import alpha.payeasebe.validators.OTPValidation;
 import alpha.payeasebe.validators.UserValidation;
 
@@ -24,6 +26,8 @@ public class OTPServicesImpl implements OTPServices {
 
     @Autowired
     UserRepository userRepository;
+     @Autowired
+    MailService mailService;
 
     @Autowired
     UserValidation userValidation;
@@ -34,30 +38,40 @@ public class OTPServicesImpl implements OTPServices {
     @Override
     public ResponseEntity<?> generateOTPCodeService(CreateOTPRequest request) {
         User user = userRepository.findByEmail(request.getEmailUser());
-
         userValidation.validateUser(user);
-        
         String otpCode = null;
 
         do {
             otpCode = generateOTP();
-        } while (otpRepository.existsByOtpCode(generateOTP())); 
+        } while 
+        (otpRepository.existsByOtpCode(generateOTP())); 
 
         OTP otp = new OTP(user, otpCode, LocalDateTime.now().plusMinutes(1));
         otpRepository.save(otp);
 
+         StringBuilder buildMessageMail = new StringBuilder();
+        buildMessageMail.append("To verify your password, please check your email by to veryfiy this otp: ");
+        buildMessageMail.append(
+                "http://localhost:5173/otp");
+        buildMessageMail.append(
+                " Your OTP CODE IS : " + otp.getOtpCode());
+        mailService.sendMail(new MailRequest(user.getEmail(), "OTP Code To Verify Your Account!", buildMessageMail.toString()));
         return ResponseHandler.responseMessage(200, "Generate OTP success!", true);
     }
 
     @Override
     public ResponseEntity<?> verifyOTPCodeService(VerifyOTPRequest request) {
         OTP otp = otpRepository.findByOtpCode(request.getOtpCode());
+
         otpValidation.validateOTP(otp);
 
         if (LocalDateTime.now().isAfter(otp.getExpiredAt()) ) {
             throw new IllegalArgumentException("OTP Code is expired");
         }
 
+        User user  = userRepository.findByEmail(request.getEmailUser());
+        user.setIsVerified(true);
+        userRepository.save(user);
         return ResponseHandler.responseMessage(200, "OTP correct", true);
     }
 
@@ -79,3 +93,33 @@ public class OTPServicesImpl implements OTPServices {
         return otp.toString();
     }
 }
+
+
+
+//  if (!userRepository.existsByEmail(request.getEmail())) {
+//             throw new NoSuchElementException("Email not found");
+//         }
+
+//         User user = userRepository.findByEmail(request.getEmail());
+
+//         if (user.getIsDeleted()) {
+//             throw new NoSuchElementException("User is not active or already deleted");
+//         }
+
+//         ResetToken resetToken = new ResetToken(user);
+
+//         LocalDateTime now = LocalDateTime.now();
+//         LocalDateTime expireDateTime = now.plusMinutes(5);
+//         resetToken.setExpiryDateTime(expireDateTime);
+
+//         resetPasswordRepository.save(resetToken);
+
+//         StringBuilder buildMessageMail = new StringBuilder();
+//         buildMessageMail.append("To reset your password, please check your email by click here: ");
+//         buildMessageMail.append(
+//                 "http://localhost:5173/create-password/" + resetToken.getToken());
+//         buildMessageMail.append(
+//                 "    /n This link is expired in 5 minutes.");
+//         mailService.sendMail(new MailRequest(user.getEmail(), "Reset your password!", buildMessageMail.toString()));
+
+//         return ResponseHandler.responseData(201, "Password Reset Succesfull!", user);
